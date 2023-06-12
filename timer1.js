317ebc339dd928f80cf311b92374d28823af5103
TJenniferS <tanishqsharma20231@gmail.com>
2023-06-12 04:18:24 -0400

    appears to work to beep for every arg after maxTime variable set to allow setting timeout for each valid arg and updates maxTime if larger value found and a final timeout set for maxTime then when reached process exits

--- timer1.js
+++ timer1.js
@@ -1,14 +1,24 @@
 const args = process.argv.slice(2);
 
 const setAlarms = function(args) {
+  let maxTime = 0;
+
   for (const arg of args) {
     const num = Number(arg);
     if (!isNaN(num) && num >= 0) {
       setTimeout(() => {
         process.stdout.write('\x07'); // System beep
       }, num * 1000);
+
+      if (num > maxTime) {
+        maxTime = num;
+      }
     }
   }
+
+  setTimeout(() => {
+    process.exit(); // Exit the process after the maximum time
+  }, maxTime * 1000);
 };
 
 setAlarms(args);
